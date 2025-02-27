98358392025da95ceb7409808bcec7518e25fd43
Shashank TS <tsshashank79@gmail.com>
2025-02-27 13:31:41 +0530

    [skip ci] deploy/load.js modified

--- deploy/load.js
+++ deploy/load.js
@@ -7,7 +7,7 @@ export const options = {
   duration: '30s',
 };
 
-const BASE_URL = 'https://app.kubesimplify.com'
+const BASE_URL = 'https://34.16.39.176.nip.io'
 
 function demo() {
   const url = `${BASE_URL}`;
